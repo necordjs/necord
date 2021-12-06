cb64cf738808e7f106272c21404cdf8e44a3fceb
SocketSomeone <sleepth228@gmail.com>
2021-12-06 13:57:12 +0200

    refactor: rename necord options constant

--- src/necord.constants.ts
+++ src/necord.constants.ts
@@ -1,6 +1,6 @@
 import { ROUTE_ARGS_METADATA } from '@nestjs/common/constants';
 
-export const MODULE_OPTIONS = 'necord:module_options';
+export const NECORD_MODULE_OPTIONS = 'necord:module_options';
 export const PARAM_ARGS_METADATA = ROUTE_ARGS_METADATA;
 
 export const APPLICATION_COMMAND_METADATA = 'necord:application_command_meta';
