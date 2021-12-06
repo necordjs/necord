8934befba7561cb77e1531709f4e6ea5b03edc5a
SocketSomeone <sleepth228@gmail.com>
2021-12-06 21:05:51 +0200

    refactor: delete junk omit properties

--- src/utils/create-necord-options.util.ts
+++ src/utils/create-necord-options.util.ts
@@ -4,7 +4,7 @@ import { OPTIONS_METADATA } from '../necord.constants';
 export function createNecordOptionDecorator<
 	T extends OptionData['type'],
 	C extends OptionMetadata<T> = OptionMetadata<T>
->(type: T, methodName: OptionTransform): (data: Omit<C, 'transform' | 'propertyKey' | 'type'>) => PropertyDecorator {
+>(type: T, methodName: OptionTransform): (data: Omit<C, 'type' | 'methodName'>) => PropertyDecorator {
 	return (data: C): PropertyDecorator => {
 		return (target: object, propertyKey: string) => {
 			const options: Record<string, C> = Reflect.getMetadata(OPTIONS_METADATA, target.constructor) || {};
