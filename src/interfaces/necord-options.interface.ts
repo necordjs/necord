a3fc88118d5dce390e25e4ed743e54d93e25afe0
SocketSomeone <sleepth228@gmail.com>
2022-02-01 03:54:45 +0200

    fix: optional development

--- src/interfaces/necord-options.interface.ts
+++ src/interfaces/necord-options.interface.ts
@@ -4,7 +4,7 @@ import { ClientOptions as DiscordClientOptions, Snowflake } from 'discord.js';
 export interface NecordModuleOptions extends DiscordClientOptions {
 	token: string;
 	prefix?: string | Function;
-	development: [Snowflake, ...Snowflake[]] | false;
+	development?: [Snowflake, ...Snowflake[]] | false;
 }
 
 export interface NecordOptionsFactory {
