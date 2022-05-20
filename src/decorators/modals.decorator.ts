7b7c165d0317bf40391daefce7ededd835cc0de9
SocketSomeone <socket.someone@gmail.com>
2022-05-20 08:19:14 +0200

    style: lint

--- src/decorators/modals.decorator.ts
+++ src/decorators/modals.decorator.ts
@@ -1,5 +1,6 @@
-import { SetMetadata } from "@nestjs/common";
-import { ModalMetadata } from "../interfaces";
-import { MODALS_METADATA } from "../necord.constants";
+import { SetMetadata } from '@nestjs/common';
+import { ModalMetadata } from '../interfaces';
+import { MODALS_METADATA } from '../necord.constants';
 
-export const Modal = (customId: string) => SetMetadata<string, ModalMetadata>(MODALS_METADATA, { customId });
+export const Modal = (customId: string) =>
+	SetMetadata<string, ModalMetadata>(MODALS_METADATA, { customId });
