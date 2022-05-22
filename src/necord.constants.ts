bff6338b9eb85f2b4ae7f8197cee9d0940cc548c
SocketSomeone <socket.someone@gmail.com>
2022-05-22 07:49:23 +0200

    chore: add  constants for subcommands meta

--- src/necord.constants.ts
+++ src/necord.constants.ts
@@ -8,12 +8,13 @@ export const SLASH_GROUP_METADATA = 'necord:slash_group_meta';
 export const MESSAGE_COMPONENT_METADATA = 'necord:message_component_meta';
 export const MODAL_METADATA = 'necord:modal_meta';
 
-export const AUTOCOMPLETE_METADATA = 'necord:autocomplete_metadata';
+export const AUTOCOMPLETE_METADATA = 'necord:autocomplete_meta';
 export const OPTIONS_METADATA = 'necord:options_meta';
 export const GUILDS_METADATA = 'necord:guilds_meta';
 
 export const MEMBER_PERMISSIONS_METADATA = 'necord:member_permissions_meta';
 export const DM_PERMISSIONS_METADATA = 'necord:dm_permissions_meta';
 
-export const SLASH_COMMANDS = 'necord:slash_commands';
-export const CONTEXT_MENUS = 'necord:context_menus';
+export const SLASH_COMMANDS = 'necord:slash_commands_meta';
+export const SLASH_SUBCOMMANDS = 'necord:slash_subcommands_meta';
+export const CONTEXT_MENUS = 'necord:context_menus_meta';
