61eb75b5530c768c79665ebe04462a61a9b63c86
SocketSomeone <socket.someone@gmail.com>
2022-05-22 07:50:52 +0200

    style: lint

--- src/necord.constants.ts
+++ src/necord.constants.ts
@@ -7,6 +7,9 @@ export const SLASH_COMMAND_METADATA = 'necord:slash_command_meta';
 export const SLASH_GROUP_METADATA = 'necord:slash_group_meta';
 export const MESSAGE_COMPONENT_METADATA = 'necord:message_component_meta';
 export const MODAL_METADATA = 'necord:modal_meta';
+export const SLASH_COMMANDS = 'necord:slash_commands_meta';
+export const SLASH_SUBCOMMANDS = 'necord:slash_subcommands_meta';
+export const CONTEXT_MENUS = 'necord:context_menus_meta';
 
 export const AUTOCOMPLETE_METADATA = 'necord:autocomplete_meta';
 export const OPTIONS_METADATA = 'necord:options_meta';
@@ -14,7 +17,3 @@ export const GUILDS_METADATA = 'necord:guilds_meta';
 
 export const MEMBER_PERMISSIONS_METADATA = 'necord:member_permissions_meta';
 export const DM_PERMISSIONS_METADATA = 'necord:dm_permissions_meta';
-
-export const SLASH_COMMANDS = 'necord:slash_commands_meta';
-export const SLASH_SUBCOMMANDS = 'necord:slash_subcommands_meta';
-export const CONTEXT_MENUS = 'necord:context_menus_meta';
