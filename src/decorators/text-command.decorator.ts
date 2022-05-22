bfa8c31f674a7912df60f3f216d113712b8d8fb7
SocketSomeone <socket.someone@gmail.com>
2022-05-22 07:45:11 +0200

    style: add lint-staged

--- src/decorators/text-command.decorator.ts
+++ src/decorators/text-command.decorator.ts
@@ -1,6 +1,6 @@
-import { SetMetadata } from "@nestjs/common";
-import { TEXT_COMMAND_METADATA } from "../necord.constants";
-import { TextCommandMeta } from "../discovery";
+import { SetMetadata } from '@nestjs/common';
+import { TEXT_COMMAND_METADATA } from '../necord.constants';
+import { TextCommandMeta } from '../discovery';
 
 export const TextCommand = (options: TextCommandMeta) =>
 	SetMetadata<string, TextCommandMeta>(TEXT_COMMAND_METADATA, options);
