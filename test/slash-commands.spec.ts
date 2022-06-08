15eb501c16da92c89dc12eee91b1b072ab27f13b
SocketSomeone <socket.someone@gmail.com>
2022-06-08 06:56:32 +0200

    test: remove old decorators

--- test/slash-commands.spec.ts
+++ test/slash-commands.spec.ts
@@ -1,10 +1,8 @@
 import { createApplication } from './utils.spec';
-import { AdminOnly, Ctx, GuildOnly, Opts, SlashCommand, SlashGroup } from '../src';
+import { Ctx, Opts, SlashCommand, SlashGroup } from '../src';
 import { CommandInteraction } from 'discord.js';
 import { LengthDto } from './dto/length.dto';
 
-@AdminOnly
-@GuildOnly
 @SlashGroup({ name: 'utils', description: 'Test group' })
 export class SlashCommandsSpec {
 	@SlashGroup({ name: 'string', description: 'Test Sub Group' })
