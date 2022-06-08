8179940f6455fe34d732036d0f1996adc02865f8
SocketSomeone <socket.someone@gmail.com>
2022-06-08 07:08:36 +0200

    feat: add nsfw property

--- src/commands/command.discovery.ts
+++ src/commands/command.discovery.ts
@@ -7,6 +7,10 @@ export interface BaseApplicationCommandMeta {
 	name_localizations?: LocalizationMap;
 	dm_permission?: boolean;
 	default_member_permissions?: PermissionResolvable;
+
+	// The functionality not released yet https://github.com/discord/discord-api-docs/pull/4965
+	// nsfw?: boolean;
+
 	/**
 	 * @deprecated Use `dm_permission` and/or `default_member_permissions` instead
 	 */
