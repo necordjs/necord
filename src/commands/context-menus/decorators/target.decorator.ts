47ccc2a7968c726c990ba5b66f54c267681bb777
SocketSomeone <socket.someone@gmail.com>
2022-09-17 23:40:35 +0300

    feat: target decorators for message, user, member

--- src/commands/context-menus/decorators/target.decorator.ts
+++ src/commands/context-menus/decorators/target.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator } from '@nestjs/common';
 import { NecordExecutionContext } from '../../../context';
 
+/**
+ * @deprecated Use `@TargetMessage`, `@TargetUser` or `@TargetMember` instead
+ */
 export const Target = createParamDecorator((_, context) => {
 	const necordContext = NecordExecutionContext.create(context);
 	const [interaction] = necordContext.getContext<'interactionCreate'>();
@@ -10,4 +13,31 @@ export const Target = createParamDecorator((_, context) => {
 	return interaction.isMessageContextMenuCommand()
 		? interaction.options.getMessage('message')
 		: interaction.options.getUser('user');
+});
+
+export const TargetMessage = createParamDecorator((_, context) => {
+	const necordContext = NecordExecutionContext.create(context);
+	const [interaction] = necordContext.getContext<'interactionCreate'>();
+
+	if (!interaction.isMessageContextMenuCommand()) return null;
+
+	return interaction.targetMessage;
+});
+
+export const TargetUser = createParamDecorator((_, context) => {
+	const necordContext = NecordExecutionContext.create(context);
+	const [interaction] = necordContext.getContext<'interactionCreate'>();
+
+	if (!interaction.isUserContextMenuCommand()) return null;
+
+	return interaction.targetUser;
+});
+
+export const TargetMember = createParamDecorator((_, context) => {
+	const necordContext = NecordExecutionContext.create(context);
+	const [interaction] = necordContext.getContext<'interactionCreate'>();
+
+	if (!interaction.isUserContextMenuCommand()) return null;
+
+	return interaction.targetMember;
 });
