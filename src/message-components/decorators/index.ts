8d2647f923c54866a643bfb642632f4819d741e1
wolffparkinson <krishanu1296@gmail.com>
2022-11-29 19:33:56 +0530

    fix(components): export select menu decorators

--- src/message-components/decorators/index.ts
+++ src/message-components/decorators/index.ts
@@ -3,3 +3,4 @@ export * from './component-param.decorator';
 export * from './message-component.decorator';
 export * from './select-menu.decorator';
 export * from './values.decorator';
+export * from './selected.decorator';
