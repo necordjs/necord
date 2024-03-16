87c42404cfcce2ec18d7b066f9789187356b98c3
Alexey Filippov <socket.someone@gmail.com>
2024-03-16 18:25:17 +0300

    Revert "feat(providers): add emojis and user (#1117)"
    This reverts commit cf36b3c15fcf2a8e64e7cb65952c6b86ee211e09.

--- src/providers/emojis.provider.ts
+++
@@ -1,8 +0,0 @@
-import { Provider } from '@nestjs/common';
-import { Client, BaseGuildEmojiManager } from 'discord.js';
-
-export const EmojisProvider: Provider<BaseGuildEmojiManager> = {
-	provide: BaseGuildEmojiManager,
-	useFactory: (client: Client) => client.emojis,
-	inject: [Client]
-};
