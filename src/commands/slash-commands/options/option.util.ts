44c921568da490a5f404186fabde229741fca984
SocketSomeone <socket.someone@gmail.com>
2025-04-27 18:32:27 +0300

    fix: remove distributive omit

--- src/commands/slash-commands/options/option.util.ts
+++ src/commands/slash-commands/options/option.util.ts
@@ -1,7 +1,6 @@
-import { APIApplicationCommandOptionBase } from 'discord-api-types/payloads/v10/_interactions/_applicationCommands/_chatInput/base';
+import { APIApplicationCommandOptionBase } from 'discord-api-types/v10';
 import { ApplicationCommandOptionType } from 'discord.js';
 import { OptionMeta } from '../slash-command.discovery';
-import { DistributiveOmit } from 'discord-api-types/utils/internals';
 
 export const OPTIONS_METADATA = 'necord:options_meta';
 
@@ -9,9 +8,7 @@ export function createOptionDecorator<T extends APIApplicationCommandOptionBase<
 	type: ApplicationCommandOptionType,
 	resolver: OptionMeta['resolver']
 ) {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	return (data: DistributiveOmit<T, 'type'>): PropertyDecorator => {
+	return (data: Omit<T, 'type'>): PropertyDecorator => {
 		return (target: any, propertyKey: string | symbol) => {
 			let metadata: Record<string, OptionMeta> = Reflect.getOwnMetadata(
 				OPTIONS_METADATA,
