d7dd36235fed28a17af79b4e0a69ed41a9c38a26
SocketSomeone <socket.someone@gmail.com>
2025-07-18 00:41:57 +0300

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ export * from './context';
 export * from './listeners';
 export * from './message-components';
 export * from './modals';
+export * from './providers';
+export * from './scopes';
 export * from './text-commands';
 
 export * from './necord.module-definition';
 export * from './necord.module';
-export * from './providers/client.provider';
 export * from './necord-explorer.service';
 export * from './necord-options.interface';
-export * from './providers/rest.provider';
