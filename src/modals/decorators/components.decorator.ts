7d2b9c931b8d5cc2421aa48cf3c010223cf94284
Abi <kpnmnd13@gmail.com>
2026-04-14 00:47:19 +0300

    refactor(modals): rename fields to modal components
    BREAKING CHANGE: Fields decorator renamed to ModalComponents, interaction.fields replaced with interaction.components

--- src/modals/decorators/components.decorator.ts
+++ src/modals/decorators/components.decorator.ts
@@ -2,12 +2,12 @@ import { createParamDecorator } from '@nestjs/common';
 import { NecordExecutionContext } from '../../context';
 
 /**
- * Represents a fields param decorator.
+ * Represents a components param decorator.
  * @url https://necord.org/interactions/modals
  * @param customId The custom id.
- * @returns The fields param decorator.
+ * @returns The components param decorator.
  */
-export const Fields = createParamDecorator((customId, context) => {
+export const ModalComponents = createParamDecorator((customId, context) => {
 	const necordContext = NecordExecutionContext.create(context);
 	const [interaction] = necordContext.getContext<'interactionCreate'>();
 	const discovery = necordContext.getDiscovery();
@@ -16,5 +16,5 @@ export const Fields = createParamDecorator((customId, context) => {
 		return null;
 	}
 
-	return customId ? interaction.fields.getTextInputValue(customId) : interaction.fields;
+	return customId ? interaction.components.getComponent(customId) : interaction.components;
 });
--- src/modals/decorators/index.ts
+++ src/modals/decorators/index.ts
@@ -1,3 +1,3 @@
-export * from './fields.decorator';
+export * from './components.decorator';
 export * from './modal.decorator';
 export * from './modal-param.decorator';
