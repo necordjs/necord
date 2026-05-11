728a773b8151acd5c9b41d67d0ea0cc4fd0ba7df
SocketSomeone <socket.someone@gmail.com>
2026-05-11 17:20:18 +0300

    style: remove trailing comma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -88,7 +88,7 @@ export default tseslint.config(
 			'@typescript-eslint/prefer-promise-reject-errors': 'off',
 			'@typescript-eslint/no-base-to-string': 'off',
 			'@typescript-eslint/unbound-method': 'off',
-			'@typescript-eslint/only-throw-error': 'off',
+			'@typescript-eslint/only-throw-error': 'off'
 		}
 	}
 );
